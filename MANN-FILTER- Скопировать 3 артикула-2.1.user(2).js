87f4430e36b4bed831e9eb389af12653d505bb87
AlwaysNeverEat <gtrixonn@gmail.com>
2026-05-21 10:55:01 +0300

    Update MANN-FILTER- Скопировать 3 артикула-2.1.user(2).js

--- MANN-FILTER- Скопировать 3 артикула-2.1.user(2).js
+++ MANN-FILTER- Скопировать 3 артикула-2.1.user(2).js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         MANN-FILTER: Скопировать 3 артикула
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.2
 // @match        *://www.mann-filter.com/*/catalog/search-results*
 // @match        *://www.mann-filter.com/*/catalog*
+// @match        *://www.mann-filter.com/*
 // @grant        GM_setClipboard
 // @updateURL    https://raw.githubusercontent.com/AlwaysNeverEat/scripts/main/MANN-FILTER-%20%D0%A1%D0%BA%D0%BE%D0%BF%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%203%20%D0%B0%D1%80%D1%82%D0%B8%D0%BA%D1%83%D0%BB%D0%B0-2.1.user(2).js
 // @downloadURL  https://raw.githubusercontent.com/AlwaysNeverEat/scripts/main/MANN-FILTER-%20%D0%A1%D0%BA%D0%BE%D0%BF%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%203%20%D0%B0%D1%80%D1%82%D0%B8%D0%BA%D1%83%D0%BB%D0%B0-2.1.user(2).js
